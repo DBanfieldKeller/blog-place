21b4037a49a6ff7ea03805370f3476686c6479db
Goat <dpajbk@gmail.com>
2022-11-08 15:54:54 -0500

    added homeroutes file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const {User, Post} = require('../models');
+
+// get all posts for home page
+
+
+// send over loggedIn session variable to homepage template
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 
 app.get("", (req, res) => {
     res.send("Hello World");
