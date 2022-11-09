b2e38be5ff78ff1e56d707cec87ce1996216a08b
Goat <dpajbk@gmail.com>
2022-11-08 19:21:01 -0500

    server.js updated with dependencies and handlebar connections

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
-      user_id: req.session.user_id,
+      userId: req.session.userId,
     });
 
     res.status(200).json(newPost);
